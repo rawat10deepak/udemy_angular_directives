f7a16cfa6c556f9872051f21096ff2e92c0d8c77
rawat10deepak <109275183+rawat10deepak@users.noreply.github.com>
2023-01-04 19:16:30 +0530

    1 file modified, 1 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './basic-highlight/better-highlight.directive';
+
 import { UnlessDirective } from './unless/unless.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
     BetterHighlightDirective,
-    UnlessDirective
+    UnlessDirective,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],
